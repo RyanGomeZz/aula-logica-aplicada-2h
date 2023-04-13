58ad777fa4f70be2390086ce902be5d17bb8b6ef
RyanGomeZz <ryangomes1103@gmail.com>
2023-04-13 08:16:50 -0300

    Criado laço while e for

--- main.java
+++ main.java
@@ -1,2 +1,24 @@
 public class main {
+
+    public static void main(String[] args){
+
+        int Contador = 0;
+
+
+
+        while (Contador < 10)
+        System.out.println("Contador =" + Contador);
+        //incremento
+        Contador = Contador + 1;
+
+
+        //laço para faça
+        for (int x = 0; x < 10; x++){
+
+            System.out.println("Contador =" + x);
+
+        }
+
+    }
+
 }
